dd8c5fbc2becedfeade1d401fda429c90dcb9da3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-03-13 01:14:00 +0200

    Fix steps test
    PR-URL: https://github.com/metarhia/web-locks/pull/9

--- test/steps.js
+++ test/steps.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const assert = require('assert').strict;
-
 const { locks } = require('..');
 
 const sleep = msec =>
   new Promise(resolve => {
-    setTimeout(resolve, msec);
+    setTimeout(() => {
+      resolve();
+    }, msec);
   });
 
 let counter = 0;
@@ -16,25 +17,32 @@ const step = current => {
   counter++;
 };
 
-module.exports = async () => {
-  (async () => {
-    step(0);
-    await locks.request('A', async () => {
-      step(1);
-      await sleep(20);
-      step(3);
-    });
-    step(4);
-  })();
-
-  (async () => {
-    await sleep(10);
-    step(2);
-    await locks.request('A', async () => {
+module.exports = () =>
+  new Promise(resolve => {
+    (async () => {
+      step(0);
+      await locks.request('A', async () => {
+        step(1);
+        await sleep(20);
+        step(3);
+      });
       step(5);
+    })();
+
+    (async () => {
       await sleep(10);
-      step(6);
-    });
-    step(7);
-  })();
-};
+      step(2);
+      await locks.request('A', async () => {
+        step(4);
+        await sleep(30);
+        step(6);
+      });
+      step(7);
+    })();
+
+    (async () => {
+      await sleep(100);
+      step(8);
+      resolve();
+    })();
+  });
