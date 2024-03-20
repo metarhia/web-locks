2fc13ddbc4a34dff80ad30a69c64d6271ae412df
Victor Korkoshko <victor.korkoshko@gmail.com>
2024-03-20 13:02:26 +0200

    Add catch and reject for the `handler` promise
    PR-URL: https://github.com/metarhia/web-locks/pull/55

---
+++ test/handler-throw-error.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert').strict;
+
+const { locks } = require('..');
+
+module.exports = async () => {
+  assert.rejects(
+    async () => {
+      await locks.request('A', async () => {
+        throw new Error('test');
+      });
+    },
+    { message: 'test' }
+  );
+};
