0fe73868a77c41f99b4581961f32df099aecea92
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2026-03-23 23:58:03 +0200

    Improve eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 
 const init = require('eslint-config-metarhia');
 
-init[0].rules['class-methods-use-this'] = 'off';
+const config = [...init];
+config[0].rules = { ...config[0].rules, 'class-methods-use-this': 'off' };
 
-module.exports = [...init];
+module.exports = config;
